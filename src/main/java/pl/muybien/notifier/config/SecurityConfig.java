dd0919b7940b2c5dd2ebb0991cea829dc67ca6c7
Milosz Rataj <miloszrataj2@gmail.com>
2024-10-15 13:51:23 +0200

    Create SecurityConfig

--- src/main/java/pl/muybien/notifier/config/SecurityConfig.java
+++ src/main/java/pl/muybien/notifier/config/SecurityConfig.java
@@ -2,10 +2,11 @@ package pl.muybien.notifier.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -18,7 +19,8 @@ public class SecurityConfig {
                     auth.requestMatchers("/").permitAll();
                     auth.anyRequest().authenticated();
                 })
-                .formLogin(Customizer.withDefaults())
+                .oauth2Login(withDefaults())
+                .formLogin(withDefaults())
                 .build();
     }
 }
