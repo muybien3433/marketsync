a4c18841607f959f261386005790eaf284a55584
Milosz Rataj <miloszrataj2@gmail.com>
2024-10-15 19:09:39 +0200

    Change Eth and Btc Service and Response to generics Delete old Bitcoin and Etherium classes

--- src/main/java/pl/muybien/notifier/authorization/HomeController.java
+++ src/main/java/pl/muybien/notifier/authorization/HomeController.java
@@ -1,4 +1,4 @@
-package pl.muybien.notifier.auth;
+package pl.muybien.notifier.authorization;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/pl/muybien/notifier/currency/crypto/bitcoin/Bitcoin.java
+++ src/main/java/pl/muybien/notifier/currency/crypto/bitcoin/Bitcoin.java
@@ -13,8 +13,16 @@ public class Bitcoin {
 
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long bitcoinId;
+        private Long btcId;
+        private String id;
+        private String rank;
         private String symbol;
         private String name;
         private Double priceUsd;
+
+        @Override
+        public String toString() {
+                return "Bitcoin{id='%s', rank='%s', symbol='%s', name='%s', priceUsd=%s}"
+                        .formatted(id, rank, symbol, name, priceUsd);
+        }
 }
