d6ee444e6827e5ec8956b7f9a6787db2308e9ec8
Milosz Rataj <miloszrataj2@gmail.com>
2024-10-17 15:49:21 +0200

    Remove Subscription package Create subscription features directly in Bitcoin class

--- src/main/java/pl/muybien/notifier/subscription/SubscriptionController.java
+++
@@ -1,9 +0,0 @@
-package pl.muybien.notifier.subscription;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class SubscriptionController {
-
-
-}
--- src/main/java/pl/muybien/notifier/subscription/SubscriptionRepository.java
+++
@@ -1,9 +0,0 @@
-package pl.muybien.notifier.subscription;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
-    Optional<Subscription> findById(Long id);
-}
