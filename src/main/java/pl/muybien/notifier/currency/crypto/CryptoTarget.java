df6ad2c386af8a7b30f582afd232fd25bf99aae3
Milosz Rataj <miloszrataj2@gmail.com>
2024-10-21 19:12:39 +0200

    Create CryptoTarget interfece to connect all Crypto Entities

---
+++ src/main/java/pl/muybien/notifier/currency/crypto/CryptoTarget.java
@@ -0,0 +1,14 @@
+package pl.muybien.notifier.currency.crypto;
+
+import pl.muybien.notifier.customer.Customer;
+
+import java.math.BigDecimal;
+
+public interface CryptoTarget {
+
+    String getName();
+    BigDecimal getPointPrice();
+    BigDecimal getUpperBoundPrice();
+    BigDecimal getLowerBoundPrice();
+    Customer getCustomer();
+}
