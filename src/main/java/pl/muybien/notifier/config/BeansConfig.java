a58ee0f513903907494cb322e527ed5673454a33
Milosz Rataj <miloszrataj2@gmail.com>
2024-10-21 19:09:36 +0200

    Add webClient to BeansConfig

--- src/main/java/pl/muybien/notifier/config/BeansConfig.java
+++ src/main/java/pl/muybien/notifier/config/BeansConfig.java
@@ -1,7 +1,16 @@
 package pl.muybien.notifier.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class BeansConfig {
+
+    @Bean
+    WebClient webClient(WebClient.Builder builder) {
+        return builder
+                .baseUrl("https://api.coincap.io/v2/assets/")
+                .build();
+    }
 }
