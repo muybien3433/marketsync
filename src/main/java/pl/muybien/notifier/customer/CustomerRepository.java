f8bb723fffe446ef2f96a6098fd9b6aba297dbe9
Milosz Rataj <miloszrataj2@gmail.com>
2024-10-23 12:32:16 +0200

    Edit CustomerRepository method

--- src/main/java/pl/muybien/notifier/customer/CustomerRepository.java
+++ src/main/java/pl/muybien/notifier/customer/CustomerRepository.java
@@ -2,5 +2,8 @@ package pl.muybien.notifier.customer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findByEmail(String email);
 }
