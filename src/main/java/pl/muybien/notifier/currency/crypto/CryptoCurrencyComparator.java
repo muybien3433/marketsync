0e9de8777a3d1c3bef7a9ce80443bbaa2bb93196
Milosz Rataj <miloszrataj2@gmail.com>
2024-10-23 12:22:11 +0200

    Create CryptoCurrencyComparator

--- src/main/java/pl/muybien/notifier/currency/crypto/CryptoCurrencyComparator.java
+++ src/main/java/pl/muybien/notifier/currency/crypto/CryptoCurrencyComparator.java
@@ -1,39 +1,56 @@
 package pl.muybien.notifier.currency.crypto;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
-import java.util.List;
 
 @Component
+@RequiredArgsConstructor
 public class CryptoCurrencyComparator {
 
     @Transactional
-    public void updateValueAndCompareWithSubscribersGoals(Crypto currentCrypto, List<CryptoTarget> subscribers) {
-        BigDecimal currentPriceUsd = currentCrypto.getPriceUsd();
-
-        subscribers.forEach(subscriber -> compareCurrentValueWithTarget(currentPriceUsd, subscriber));
-    }
-
-    private <T extends CryptoTarget> void compareCurrentValueWithTarget(BigDecimal currentPriceUsd, T cryptoTarget) {
-        if (cryptoTarget != null) {
-            System.out.println("Subscriber is not null"); // TODO: delete
-            System.out.println("Current value usd: " + currentPriceUsd);
-            BigDecimal upperTargetPrice = cryptoTarget.getUpperBoundPrice();
-            BigDecimal lowerTargetPrice = cryptoTarget.getLowerBoundPrice();
+    public  <T extends CryptoTarget> void compareValueWithSubscriptionAndNotifyIfNecessary(BigDecimal currentPriceUsd, T subscription) {
+        if (subscription != null) {
+            BigDecimal upperTargetPrice = subscription.getUpperBoundPrice();
+            BigDecimal lowerTargetPrice = subscription.getLowerBoundPrice();
             boolean currentValueEqualsOrGraterThanTarget = currentPriceUsd.compareTo(upperTargetPrice) >= 0;
             boolean currentValueEqualsOrLowerThanTarget = currentPriceUsd.compareTo(lowerTargetPrice) <= 0;
 
             if (currentValueEqualsOrGraterThanTarget) {
                 // TODO: Send notification via email instead
                 System.out.println("Current value: " + currentPriceUsd + " is >= than target: " + upperTargetPrice);
+
+//                cryptoSubscriptionManager.removeSubscription(subscription, subscription.getName());
             } else if (currentValueEqualsOrLowerThanTarget) {
                 // TODO: Send notification via email instead
                 System.out.println("Current value: " + currentPriceUsd + " is <= target: " + lowerTargetPrice);
+//                cryptoService.removeSubscription(subscription);
             }
         } else {
             System.out.println("Subscriber is null"); // TODO: create SubscriberNotFoundException
         }
+    }
+
+    public <T extends CryptoTarget> boolean currentPriceMetSubscriptionCondition(BigDecimal currentPriceUsd, T subscription) {
+        System.out.println("Current value: " + currentPriceUsd);
+        if (subscription != null) {
+            BigDecimal upperTargetPrice = subscription.getUpperBoundPrice();
+            BigDecimal lowerTargetPrice = subscription.getLowerBoundPrice();
+            boolean currentValueEqualsOrGraterThanTarget = currentPriceUsd.compareTo(upperTargetPrice) >= 0;
+            boolean currentValueEqualsOrLowerThanTarget = currentPriceUsd.compareTo(lowerTargetPrice) <= 0;
+
+            if (currentValueEqualsOrGraterThanTarget) {
+                // TODO: Send notification via email instead
+                System.out.println("Current value: " + currentPriceUsd + " is >= than target: " + upperTargetPrice);
+                return true;
+            } else if (currentValueEqualsOrLowerThanTarget) {
+                // TODO: Send notification via email instead
+                System.out.println("Current value: " + currentPriceUsd + " is <= target: " + lowerTargetPrice);
+                return true;
+            }
+        }
+        return false;
     }
 }
