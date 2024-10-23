5515db72d4c8bd306830ed7f0497e41768bc50d8
Milosz Rataj <miloszrataj2@gmail.com>
2024-10-23 12:32:16 +0200

    Edit CustomerRepository method

--- src/main/java/pl/muybien/notifier/currency/crypto/CryptoTarget.java
+++ src/main/java/pl/muybien/notifier/currency/crypto/CryptoTarget.java
@@ -7,6 +7,7 @@ import java.math.BigDecimal;
 public interface CryptoTarget {
 
     String getName();
+    Long getId();
     BigDecimal getUpperBoundPrice();
     BigDecimal getLowerBoundPrice();
     Customer getCustomer();
