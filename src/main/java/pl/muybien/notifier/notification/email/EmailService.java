a90d954eab137c7664c3ff2c9dc4034569611543
Milosz Rataj <miloszrataj2@gmail.com>
2024-10-24 17:50:05 +0200

    Edit EmailService and CryptoCurrencyComparator to send emails

--- src/main/java/pl/muybien/notifier/notification/email/EmailService.java
+++ src/main/java/pl/muybien/notifier/notification/email/EmailService.java
@@ -1,4 +1,24 @@
 package pl.muybien.notifier.notification.email;
 
-public class EmailService {
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+import pl.muybien.notifier.notification.NotificationService;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService implements NotificationService {
+
+    private final JavaMailSender mailSender;
+
+    public void sendEmail(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("your.notificationer@gmail.com");
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+
+        mailSender.send(message);
+    }
 }
