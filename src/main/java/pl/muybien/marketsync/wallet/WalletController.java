cc2d793e4bfa88eabb0ea68be6d5a4450c2e7326
Milosz Rataj <miloszrataj2@gmail.com>
2024-10-27 17:26:58 +0100

    Create waller feature

--- src/main/java/pl/muybien/marketsync/wallet/WalletController.java
+++ src/main/java/pl/muybien/marketsync/wallet/WalletController.java
@@ -1,4 +1,13 @@
 package pl.muybien.marketsync.wallet;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/wallet")
+@RequiredArgsConstructor
 public class WalletController {
+
+    private final WalletService walletService;
 }
