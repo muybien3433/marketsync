2c92cc3d56b30c34139ef31228a25f2574531f0e
Milosz Rataj <miloszrataj2@gmail.com>
2024-10-27 17:26:58 +0100

    Create waller feature

--- src/main/java/pl/muybien/marketsync/wallet/WalletService.java
+++ src/main/java/pl/muybien/marketsync/wallet/WalletService.java
@@ -1,8 +1,12 @@
 package pl.muybien.marketsync.wallet;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import pl.muybien.marketsync.finance.FinanceProvider;
+
+import java.math.BigDecimal;
 
 @Service
 @RequiredArgsConstructor
@@ -11,10 +15,11 @@ public class WalletService {
     private final WalletRepository walletRepository;
     private final FinanceProvider financeProvider;
 
+//    @Transactional
 //    public void addNewAssetToWallet(OidcUser oidcUser, String uri, BigDecimal count) {
 //        BigDecimal currentPrice =  assetProvider.fetchFinance(uri).getPriceUsd();
 //        Wallet wallet = Wallet.builder()
-//                .assetName(uri)
+//                .assetName(uri.toLowerCase())
 //                .count(count)
 //                .currentPrice(currentPrice)
 //                .investmentPeriod()
