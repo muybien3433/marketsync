4458865993225e813aaf5e93b273b7f7edcc2567
Milosz Rataj <miloszrataj2@gmail.com>
2024-10-27 15:00:58 +0100

    Remove unnecessary variables, and move them to Asset

--- src/main/java/pl/muybien/marketsync/wallet/Wallet.java
+++ src/main/java/pl/muybien/marketsync/wallet/Wallet.java
@@ -2,11 +2,9 @@ package pl.muybien.marketsync.wallet;
 
 import jakarta.persistence.*;
 import lombok.*;
+import pl.muybien.marketsync.asset.Asset;
 import pl.muybien.marketsync.customer.Customer;
 
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
@@ -19,22 +17,12 @@ import java.util.List;
 public class Wallet {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    private String financeName;
-    private BigDecimal count;
-    private BigDecimal currentPrice;
-    private Integer investmentPeriod;
-    private Double profitInPercentage;
-    private BigDecimal profit;
-    private String customerEmail;
-    private LocalDateTime creationDate;
-
-    @ElementCollection(fetch = FetchType.EAGER)
-    private List<BigInteger> averagePurchasePrice;
 
     @OneToOne
     private Customer customer;
 
+    @OneToMany(mappedBy = "wallet", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private List<Asset> assets;
 }
