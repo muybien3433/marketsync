7e41ed613f1b4dd9b8d9cc5f9394bc11a10a60bb
Milosz Rataj <miloszrataj2@gmail.com>
2024-10-27 15:00:03 +0100

    Create Asset entity

--- src/main/java/pl/muybien/marketsync/asset/Asset.java
+++ src/main/java/pl/muybien/marketsync/asset/Asset.java
@@ -1,4 +1,27 @@
 package pl.muybien.marketsync.asset;
 
+import jakarta.persistence.*;
+import pl.muybien.marketsync.wallet.Wallet;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "asset")
 public class Asset {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private BigDecimal value;
+    private BigDecimal count;
+    private BigDecimal averagePurchasePrice;
+    private Integer purchaseCount;
+    private BigDecimal currentPrice;
+    private Integer investmentPeriodInDays;
+    private BigDecimal profitInPercentage;
+    private BigDecimal profit;
+
+    @ManyToOne
+    private Wallet wallet;
 }
