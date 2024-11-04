8656918db93781ad494f9fa69a6b0eeccd5dae41
Milosz Rataj <miloszrataj2@gmail.com>
2024-11-04 22:58:06 +0100

    Edit wallet components

--- marketsync-frontend/src/app/app.routes.ts
+++ marketsync-frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import {AppComponent} from './app.component';
 import {WalletComponent} from './wallet/wallet.component';
+import {LoginComponent} from './login/login.component';
 
 export const routes: Routes = [
   { path: '', component: AppComponent },
   { path: 'wallet', component: WalletComponent },
+  { path: 'login', component: LoginComponent },
 ];
