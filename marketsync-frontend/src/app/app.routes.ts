c5a4a0525464f21c995745bff6927ecd8e252ab6
Milosz Rataj <miloszrataj2@gmail.com>
2024-11-05 13:52:55 +0100

    Move frontend to new-frontend branch

--- marketsync-frontend/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import {AppComponent} from './app.component';
-import {WalletComponent} from './wallet/wallet.component';
-
-export const routes: Routes = [
-  { path: '', component: AppComponent },
-  { path: 'wallet', component: WalletComponent },
-];
