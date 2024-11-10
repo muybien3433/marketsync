1ea439bb54ec9dff6b1242a45d1c9aa066bfde22
Milosz Rataj <miloszrataj2@gmail.com>
2024-11-10 16:40:42 +0100

    Create email microservice

--- marketsync-backend/main/src/main/java/pl/muybien/marketsync/finance/crypto/steth/StethRepository.java
+++
@@ -1,6 +0,0 @@
-package pl.muybien.marketsync.finance.crypto.steth;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StethRepository extends JpaRepository<Steth, Long> {
-}
