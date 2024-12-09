ee090764eb68eeb7cde7abcd0c2eadbeced99a3e
muybien <miloszrataj2@gmail.com>
2024-12-09 14:33:34 +0100

    Set keycloak in backend

--- backend/services/gateway/src/main/java/pl/muybien/gateway/config/SecurityConfig.java
+++ backend/services/gateway/src/main/java/pl/muybien/gateway/config/SecurityConfig.java
@@ -1,21 +1,14 @@
 package pl.muybien.gateway.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
-import org.springframework.security.oauth2.jwt.ReactiveJwtDecoders;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
-
-    @Value("${spring.jwt.decoder.issuer-url}")
-    private String issuerUrl;
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
@@ -29,14 +22,11 @@ public class SecurityConfig {
                         .anyExchange()
                         .authenticated()
                 )
-                .oauth2ResourceServer((oauth2) -> oauth2
-                        .jwt(Customizer.withDefaults())
+                .oauth2ResourceServer((oauth2) ->
+                        oauth2.jwt(token ->
+                                token.jwtAuthenticationConverter(
+                                        new KeycloakJwtAuthenticationConverter()))
                 );
         return http.build();
-    }
-
-    @Bean
-    public ReactiveJwtDecoder jwtDecoder() {
-        return ReactiveJwtDecoders.fromIssuerLocation(issuerUrl);
     }
 }
