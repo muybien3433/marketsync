88141ebd03e4866587afa7e9259bd22c16ada60d
muybien <miloszrataj2@gmail.com>
2025-03-14 13:04:21 +0100

    Remove spaces

--- backend/services/wallet/src/main/java/pl/muybien/asset/AssetService.java
+++ backend/services/wallet/src/main/java/pl/muybien/asset/AssetService.java
@@ -2,7 +2,6 @@ package pl.muybien.asset;
 
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import pl.muybien.asset.dto.AssetAggregateDTO;
