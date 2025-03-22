d581b3289840370608dac69d4668950476861007
muybien <miloszrataj2@gmail.com>
2025-03-22 12:00:59 +0100

    Update to angular 19 & clean project

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -5,22 +5,20 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatListModule} from '@angular/material/list';
 import {MatCardModule} from '@angular/material/card';
 import {ClipboardModule} from '@angular/cdk/clipboard';
-import {HomeComponent} from "./pages/home/home.component";
 
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet,
-    MatGridListModule,
-    MatButtonModule,
-    MatListModule,
-    MatCardModule,
-    ClipboardModule,
-    RouterModule, HomeComponent
-  ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    imports: [RouterOutlet,
+        MatGridListModule,
+        MatButtonModule,
+        MatListModule,
+        MatCardModule,
+        ClipboardModule,
+        RouterModule,
+    ],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
 }
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,20 +1,7 @@
 import {Routes} from '@angular/router';
 import {AuthGuard} from './services/auth.guard';
-import {HomeComponent} from "./pages/home/home.component";
-import {WalletComponent} from "./pages/wallet/wallet.component";
-import {WalletAddAssetComponent} from "./pages/wallet/wallet-add-asset/wallet-add-asset.component";
-import {WalletEditAssetComponent} from "./pages/wallet/wallet-edit-asset/wallet-edit-asset.component";
-import {WalletAssetHistoryComponent} from "./pages/wallet/wallet-asset-history/wallet-asset-history.component";
-import {SubscriptionComponent} from "./pages/subscription/subscription.component";
-import {SubscriptionAddComponent} from "./pages/subscription/subscription-add/subscription-add.component";
-
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent},
-    {path: 'wallet', component: WalletComponent, canActivate: [AuthGuard]},
-    {path: 'wallet-add-asset', component: WalletAddAssetComponent, canActivate: [AuthGuard]},
-    {path: 'wallet-edit-asset', component: WalletEditAssetComponent, canActivate: [AuthGuard]},
-    {path: 'wallet-asset-history', component: WalletAssetHistoryComponent, canActivate: [AuthGuard]},
-    {path: 'subscription', component: SubscriptionComponent, canActivate: [AuthGuard]},
-    {path: 'subscription-add', component: SubscriptionAddComponent, canActivate: [AuthGuard]},
+    // {path: '', component: HomeComponent},
+    // {path: 'wallet', component: WalletComponent, canActivate: [AuthGuard]},
 ];
