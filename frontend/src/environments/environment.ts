ecf3f6397e06905fdda4671bac1c78a6139b4694
muybien <miloszrataj2@gmail.com>
2025-03-27 16:45:42 +0100

    Change logo styling

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,19 +1,10 @@
 import packageInfo from "../../package.json";
 
-// export const environment = {
-//     appVersion: packageInfo.version,
-//     production: false,
-//     baseUrl: 'http://localhost/api/v1',
-//     keycloakUrl: 'http://localhost:8080',
-//     keycloakRealm: 'master',
-//     keycloakClientId: 'marketsync-client'
-// }
-
 export const environment = {
     appVersion: packageInfo.version,
     production: false,
-    baseUrl: 'https://muybien.pl/api/v1',
-    keycloakUrl: 'https://auth.muybien.pl',
+    baseUrl: 'http://localhost/api/v1',
+    keycloakUrl: 'http://localhost:8080',
     keycloakRealm: 'master',
     keycloakClientId: 'marketsync-client'
 }
