8362566b8258a256b097e236a53bc9f947adc30d
muybien <miloszrataj2@gmail.com>
2025-05-08 22:56:02 +0200

    Change HttpServletRequest to ServerHttpRequest in gateway GlobalExceptionHandler methods

--- backend/gateway/src/main/java/pl/muybien/exception/GlobalExceptionHandler.java
+++ backend/gateway/src/main/java/pl/muybien/exception/GlobalExceptionHandler.java
@@ -1,8 +1,8 @@
 package pl.muybien.exception;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -13,26 +13,26 @@ public class GlobalExceptionHandler {
 
     //                          Customer
     @ExceptionHandler(CustomerNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleCustomerNotFoundException(CustomerNotFoundException e, HttpServletRequest r) {
+    public ResponseEntity<ErrorResponse> handleCustomerNotFoundException(CustomerNotFoundException e, ServerHttpRequest r) {
         ErrorResponse response = new ErrorResponse(
                 LocalDateTime.now().toString(),
                 404,
                 e.getMessage(),
                 "NOT_FOUND",
-                r.getRequestURI()
+                r.getURI().getPath()
         );
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
     }
 
     //                          Other
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e, HttpServletRequest r) {
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e, ServerHttpRequest r) {
         ErrorResponse response = new ErrorResponse(
                 LocalDateTime.now().toString(),
                 400,
                 e.getMessage(),
                 "BAD_REQUEST",
-                r.getRequestURI()
+                r.getURI().getPath()
         );
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
