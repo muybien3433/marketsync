768d8151c11d8ec84f2b8c6ab482932eb223e17c
muybien <miloszrataj2@gmail.com>
2025-12-10 20:27:11 +0100

    Fix dividing by 0

--- backend/wallet/src/main/java/pl/muybien/wallet/asset/AssetRepository.java
+++ backend/wallet/src/main/java/pl/muybien/wallet/asset/AssetRepository.java
@@ -23,12 +23,20 @@ public interface AssetRepository extends JpaRepository<Asset, UUID> {
                         a.assetType,
                         a.unitType,
                         CAST(SUM(a.count) AS BIGDECIMAL),
-                        CAST(SUM(a.purchasePrice * a.count) / SUM(a.count) AS BIGDECIMAL),
+                        CAST(
+                            CASE\s
+                                WHEN SUM(a.count) = 0 THEN 0
+                                ELSE SUM(a.purchasePrice * a.count) / SUM(a.count)
+                            END
+                            AS BIGDECIMAL
+                        ),
                         CAST(a.currentPrice AS BIGDECIMAL),
                         a.currencyType,
                         a.customerId)
                     FROM Asset a
                     WHERE a.customerId = :customerId
-                    GROUP BY a.name, a.symbol, a.uri, a.assetType, a.unitType, a.currentPrice, a.currencyType, a.customerId""")
+                    GROUP BY a.name, a.symbol, a.uri, a.assetType, a.unitType, a.currentPrice, a.currencyType, a.customerId
+                   \s""")
     Optional<List<AssetGroupDTO>> findAndAggregateAssetsByCustomerId(@Param("customerId") UUID customerId);
+
 }
