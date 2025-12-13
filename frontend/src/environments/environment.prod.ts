5e9e87da0a482013f9ca4d57ca0aef1301df124e
muybien <miloszrataj2@gmail.com>
2025-12-13 16:40:22 +0100

    Change env file

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
     appVersion: packageInfo.version,
     production: true,
     baseUrl: 'https://muybien.pl/api/v1',
-    keycloakUrl: 'https://auth.muybien.pl,
+    keycloakUrl: 'https://auth.muybien.pl',
     keycloakRealm: 'marketsync-client',
     keycloakClientId: 'frontend-client',
     wsWalletUrl: 'https://muybien.pl/ws-wallet'
