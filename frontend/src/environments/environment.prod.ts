de8915100b259eccd2ce3c55ed646338c18d5f71
muybien <miloszrataj2@gmail.com>
2025-12-13 13:15:10 +0100

    Add prod env

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
-import packageInfo from "../../package.json";
+import packageInfo from '../../package.json';
 
 export const environment = {
     appVersion: packageInfo.version,
-    production: false,
-    baseUrl: 'http://localhost:9999/api/v1',
+    production: true,
+    baseUrl: 'http://gateway-server:9999/api/v1',
     keycloakUrl: 'http://localhost:8080',
     keycloakRealm: 'marketsync-client',
     keycloakClientId: 'frontend-client',
-    wsWalletUrl: 'http://localhost:9999/ws-wallet'
-}
+    wsWalletUrl: 'http://gateway-server:9999/ws-wallet'
+};
