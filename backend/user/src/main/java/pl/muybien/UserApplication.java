5e2e7c313ce0cef7972e1f947029d89860aa2f41
muybien <miloszrataj2@gmail.com>
2025-12-14 19:04:38 +0100

    Init iam and user microservice

---
+++ backend/user/src/main/java/pl/muybien/UserApplication.java
@@ -0,0 +1,12 @@
+package pl.muybien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IamApplication {
+
+    static void main(String[] args) {
+		SpringApplication.run(IamApplication.class, args);
+	}
+}
